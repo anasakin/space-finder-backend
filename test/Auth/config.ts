f4c2f198ceff0af5b88ac2733bc6394bb8c29438
Aleksei Nasakin <alexey.nasakin@gmail.com>
2023-01-11 18:15:05 +0100

    Cognito Auth start

---
+++ test/Auth/config.ts
@@ -0,0 +1,9 @@
+
+
+export const config = {
+    REGION: 'eu-central-1',
+    USER_POOL_ID: 'eu-central-1_Nar6G0Dym',
+    APP_CLIENT_ID: '2v460qik7i86jp7sdj7lt9f9iv',
+    TEST_USER_NAME: "anasakin",
+    TEST_USER_PASSWORD: "YEDut3$9eXN"
+}
