e62811d65d53b0156312034a7819eb1c0aab34cc
Aleksei Nasakin <alexey.nasakin@gmail.com>
2023-01-12 19:32:53 +0100

    Authorizer + UserPool

--- test/Auth/config.ts
+++ test/Auth/config.ts
@@ -2,8 +2,8 @@
 
 export const config = {
     REGION: 'eu-central-1',
-    USER_POOL_ID: 'eu-central-1_Nar6G0Dym',
-    APP_CLIENT_ID: '2v460qik7i86jp7sdj7lt9f9iv',
+    USER_POOL_ID: 'eu-central-1_K5FTxhkXn',
+    APP_CLIENT_ID: '5oca3bo1grnnuijl7r9n7qlj9p',
     TEST_USER_NAME: "anasakin",
     TEST_USER_PASSWORD: "YEDut3$9eXN"
 }
